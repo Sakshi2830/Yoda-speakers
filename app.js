822cb1c734a74e3144d7574b0e9bf626418c9fd6
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-08-31 15:58:48 +0530

    addinfg api and javascript

---
+++ app.js
@@ -0,0 +1,29 @@
+var btntranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var output = document.querySelector("#output-here");
+
+var serverUrl = "https://api.funtranslations.com/translate/yoda.json";
+
+function getTransUrl(text) {
+    return serverUrl + "?" + "text=" + text;
+    
+}
+
+function errorHandler(err) {
+     console.log("Error Occurred:" + err);
+     alert("Something Went Wrong");   
+}
+btntranslate.addEventListener("click",function clickHandler() {
+      //taking Inputs
+    var inputText = txtInput.value;
+    //calling Server
+    fetch(getTransUrl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var transText = json.contents.translated;
+        output.innerHTML = transText;
+    })
+
+    .catch(errorHandler)
+
+});
